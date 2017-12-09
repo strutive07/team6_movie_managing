e568801f8ef84de436c39921758f34b54adef4ab
moontaijin <moontaijin@naver.com>
2017-12-09 22:37:40 +0900

    Update option.c
    delete옵션 인자 추가

--- option.c
+++ option.c
@@ -3353,7 +3353,7 @@ int count_movie_list(struct movie* movie)
 	return cnt;
 }
 
-void option_delete_actor(int num,ACTOR actor)
+void option_delete_actor(int num,ACTOR actor,bool from_where)
 {
   ACTOR actor_tmp=actor;
   ACTOR actor_tmp2=actor;
@@ -3439,11 +3439,14 @@ void option_delete_actor(int num,ACTOR actor)
 
     printf("Delete Complete!\n");
 
-    FILE *actor_log_write;
-    actor_log_write=fopen("actor_log.txt","a");
-    fprintf(actor_log_write,"delete:%d::::\n",num);
-    fclose(actor_log_write);
-    fprint_list_movie_director_actor('A',public_first_movie,public_director,public_actor);
+    if (!from_where)
+    {
+      FILE *actor_log_write;
+      actor_log_write=fopen("actor_log.txt","a");
+      fprintf(actor_log_write,"delete:%d::::\n",num);
+      fclose(actor_log_write);
+      fprint_list_movie_director_actor('A',public_first_movie,public_director,public_actor);
+    }
   }
   else
   {
@@ -3451,7 +3454,7 @@ void option_delete_actor(int num,ACTOR actor)
   }
 }
 
-void option_delete_director(int num,DIRECTOR director)
+void option_delete_director(int num,DIRECTOR director,bool from_where)
 {
   DIRECTOR director_tmp=director;
   DIRECTOR director_tmp2=director;
@@ -3533,11 +3536,14 @@ void option_delete_director(int num,DIRECTOR director)
 
     printf("Delete Complete!\n");
 
-    FILE *director_log_write;
-    director_log_write=fopen("director_log.txt","a");
-    fprintf(director_log_write,"delete:%d::::\n",num);
-    fclose(director_log_write);
-    fprint_list_movie_director_actor('D',public_first_movie,public_director,public_actor);
+    if (!from_where)
+    {
+      FILE *director_log_write;
+      director_log_write=fopen("director_log.txt","a");
+      fprintf(director_log_write,"delete:%d::::\n",num);
+      fclose(director_log_write);
+      fprint_list_movie_director_actor('D',public_first_movie,public_director,public_actor);
+    }
   }
   else
   {
@@ -3546,7 +3552,7 @@ void option_delete_director(int num,DIRECTOR director)
 
 }
 
-void option_delete_movie(int num,struct movie* movie)
+void option_delete_movie(int num,struct movie* movie,bool from_where)
 {
   struct movie* movie_tmp=movie;
   struct movie* movie_tmp2=movie;
@@ -3634,12 +3640,14 @@ void option_delete_movie(int num,struct movie* movie)
 
     printf("Delete Complete!\n");
 
-    FILE *movie_log_write;
-    movie_log_write=fopen("movie_log.txt","a");
-    fprintf(movie_log_write,"delete:%d::::\n",num);
-    fclose(movie_log_write);
-
-    fprint_list_movie_director_actor('M',public_first_movie,public_director,public_actor);
+    if (!from_where)
+    {
+      FILE *movie_log_write;
+      movie_log_write=fopen("movie_log.txt","a");
+      fprintf(movie_log_write,"delete:%d::::\n",num);
+      fclose(movie_log_write);
+      fprint_list_movie_director_actor('M',public_first_movie,public_director,public_actor);
+    }
   }
   else
   {
